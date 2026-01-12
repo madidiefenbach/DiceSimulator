97391e22fd29ceda4a56217bf74a40d32936052a
madidiefenbach <madidiefenbach@gmail.com>
2026-01-12 11:29:16 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DiceSimulator
+{
+    public class DiceRoller
+    {
+        private readonly Random _random = new Random();
+        // This method simulates rolling two dice numRolls times
+        // and returns an array where indexes 2..12 store counts.
+        public int[] RollDice(int numRolls)
+        {
+            int[] results = new int[13]; // 0...12
+            
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = _random.Next(1, 7); // 1-6
+                int die2 = _random.Next(1, 7); // 1-6
+                int sum = die1 + die2;
+
+                results[sum]++;
+            }
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Madison Diefenbach 
+
+using System; 
+
+namespace DiceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate?");
+
+            int numRolls;
+            while (!int.TryParse(Console.ReadLine(), out numRolls) || numRolls <= 0)
+            {
+                Console.Write("Please enter a positive whole number: ");
+            }
+
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDice(numRolls);
+
+            Console.WriteLine();
+            Console.WriteLine("Dice Rolling Simulation Results");
+            Console.WriteLine("Each \"*\" represets 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {numRolls}.");
+            Console.WriteLine();
+
+            for (int sum = 2; sum <= 12; sum ++)
+            {
+                int count = results[sum];
+                double percentage = (count / (double)numRolls) * 100;
+
+                // 1 star per 1%
+                int stars = (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
+
+                Console.Write($"{sum}: ");
+                Console.WriteLine(new string('*', stars));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+        }
+    }
+}
